0e08838e83ae8073c956f2302fcc0c4d88b0447f
nitin gupta <nitingupta40@gmail.com>
2019-06-30 12:32:53 +1000

    Added Pin Schema
    Added Pin Schema

--- typeDefs.js
+++ typeDefs.js
@@ -7,6 +7,24 @@ module.exports = gql `
             email: String
             picture: String
         }
+
+        type Pin {
+            _id: ID
+            createdAt: String
+            title: String 
+            content: String
+            image: String
+            latitude: Float
+            longitude: Float
+            author: User
+            comments: [Comment]
+        }
+
+        type Comment {
+            text: String
+            createdAt: String
+            author: User
+        }
         
         type Query {
             me: User
