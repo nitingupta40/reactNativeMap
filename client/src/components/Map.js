e24ce4ef62758ac772895aebb25acdaf42103430
nitin gupta <nitingupta40@gmail.com>
2019-07-07 13:00:36 +1000

    Map Pin Added
    Map Pin Added

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 import PinIcon from "./PinIcon";
+import Context from "../context";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -13,6 +14,7 @@ const INITIAL_VIEWPORT = {
 };
 
 const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context);
   const [viewport, setViewPort] = useState(INITIAL_VIEWPORT); 
   const [userPosition, setUserPosition] = useState(null);
 
@@ -32,6 +34,21 @@ const Map = ({ classes }) => {
       }
   };
 
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if(!leftButton) {
+      return
+    }
+    if(!state.draft){
+      dispatch({ type: "CREATE_DRAFT" });
+    }
+
+    const [longitude, latitude] = lngLat;
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude }
+    })
+  };
+
   return (
           <div className={classes.root}>
             <ReactMapGL
@@ -39,6 +56,8 @@ const Map = ({ classes }) => {
               height="calc(100vh - 64px)"
               mapStyle="mapbox://styles/mapbox/streets-v9"
               mapboxApiAccessToken="pk.eyJ1Ijoibml0aW5ndXB0YTQwIiwiYSI6ImNqeGhxZXE5dTBmd2EzcG85Y3lua3dxejEifQ.Apx5cCkvN8iGAUEP4P8QGg"
+              onViewportChange={newViewport => setViewPort(newViewport)}
+              onClick={ handleMapClick }
               {...viewport}>
                 <div className={classes.navigationControl}>
                   <NavigationControl
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -15,7 +15,20 @@ export default function reducer(state, {type, payload}){
                 ...state,
                 isAuth: false,
                 currentUser: null
-            }
+            };
+        case "CREATE_DRAFT": 
+            return {
+                ...state,
+                draft: {
+                    latitude: 0,
+                    longitude: 0
+                }
+            };
+        case "UPDATE_DRAFT_LOCATION":
+            return {
+                ...state,
+                draft: payload
+            }    
         default :
             return state;
     }
